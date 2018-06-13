806280dbff2329e9ca0110ecc85d81a9b4c6a2fd
Kailash <kailashuniyal04@gmail.com>
2018-06-13 15:53:48 +0530

    Test Prog & An array prob

---
+++ Test Prog/array3.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a = 0;
+	int* b = &a;
+	cout<<"size of b "<<sizeof(b);
+}
---
+++ Test Prog/fucntion1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void Change(int *a){
+	*a=3;
+}
+
+int main(){
+	int a = 2;
+	Change(&a);
+	cout<<a;
+}
