7661e7e91f1dcf81e3d5ac062cb0452027340e15
Kailash <kailashuniyal04@gmail.com>
2018-06-17 02:10:11 +0530

    vector and string

---
+++ Array & Strings/rotate_matrix_code_ninja.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+void transpose(int **input, int n){
+	for(int i=0;i<n;i++){
+		for(int j=i;j<n;j++){
+			int temp =	input[j][i];
+			input[j][i] = input[i][j];
+			input[i][j] = temp;
+		}
+	}
+}
+
+void rotate(int **input, int n){
+	transpose(input,n);
+	for(int j=0;j<n;j++){
+		for(int i=0,k=n-1;i<k;i++,k--){
+			int temp =	input[i][j];
+			input[i][j] = input[k][j];
+			input[k][j] = temp;
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cout<<input[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+int main(){
+	int n;
+	cin>>n;
+	int count=0;
+
+	int **array = (int **) malloc (n*sizeof(int*));
+	for(int i=0;i<n;i++){
+		array[i] = (int *) malloc(n*sizeof(int));
+	}
+
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			array[i][j]=count++;
+		}
+	}
+
+	rotate(array,n);
+
+}
---
+++ Test Prog/vector_2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+	vector<int> v(4,12);
+	v.push_back(15);
+	cout << v.size() <<""<< v.capacity();
+}
