0c4e9aae2803c47f94615b5102af652c48c2271e
Kailash <kailashuniyal04@gmail.com>
2018-06-25 16:58:19 +0530

    Implementing own hashmap using separate chaining (upto insertion).

---
+++ Test Prog/templates.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//Generic datatype
+template<class T>
+T addcrap(T a, T b){
+	return a+b;
+}
+
+int main(){
+	string x="a",y="b",z;
+	z = addcrap(x,y);
+	int k=0,s=3,t;
+	t = addcrap(k,s);
+	cout<<z<<endl;
+	cout<<t<<endl;
+}
