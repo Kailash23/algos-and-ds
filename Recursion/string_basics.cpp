708bdbd3888e90d09e99fba94ac6bb71c5dbf779
Kailash <kailashuniyal04@gmail.com>
2018-07-13 23:38:32 +0530

    string subsequences

---
+++ Recursion/string_basics.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string* sp = new string;
+	*sp = "def";
+	cout<<sp<<endl;		//0x756ca0
+	cout<<*sp<<endl;	//def
+
+	string str2;
+	getline(cin,str2);
+	cout<<str2<<endl;
+
+	string s = "abc";
+	s+=str2;
+	cout<<s<<endl;
+
+	cout<<s.size()<<endl;
+	cout<<s.length()<<endl;
+
+	string str1 = "abcdefgh";
+	cout<<str1.substr(3)<<endl;		//defgh
+	cout<<str1.substr(3,3)<<endl;		//def
+
+	cout<<str1.find("def")<<endl;		//3
+
+}
